d6713c88e55e72378cd8e1398e0b9e6288b77666
wukailing <979583477@qq.com>
2018-01-11 09:58:24 +0800

    springboot:alipay init

---
+++ springboot-templates/src/main/java/springboot/SpringbootTemplatesApplication.java
@@ -0,0 +1,12 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTemplatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTemplatesApplication.class, args);
+	}
+}
---
+++ springboot-templates/src/main/java/springboot/controller/WelcomeController.java
@@ -0,0 +1,21 @@
+package springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/** 模板使用控制器
+ * Created by Sawyer on 2018/1/11.
+ */
+@Controller
+public class WelcomeController {
+
+    private String message = "Hello World";
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "welcome";
+    }
+}
