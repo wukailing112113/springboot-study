45b2399b091d2f956dd39553c37ebf255e91aea9
wukailing <979583477@qq.com>
2018-01-11 10:03:11 +0800

    springboot:templates init

--- springboot-templates/src/main/java/springboot/controller/WelcomeController.java
+++ springboot-templates/src/main/java/springboot/controller/WelcomeController.java
@@ -13,7 +13,7 @@ public class WelcomeController {
 
     private String message = "Hello World";
 
-    @RequestMapping("/")
+    @RequestMapping("/welcome")
     public String welcome(Map<String, Object> model) {
         model.put("message", this.message);
         return "welcome";
